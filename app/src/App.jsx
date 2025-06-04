a649f92d78952c4f58ae895b81ceecfc19eff1bc
chokopay <vazovmikola@gmail.com>
2025-06-04 20:08:28 +0200

    react-tabels

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,4 +1,3 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Lesson1 from "./pages/Lesson1";
@@ -7,11 +6,13 @@ import Lesson2 from "./pages/Lesson2";
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/lesson/1" element={<Lesson1 />} />
-        <Route path="/lesson/2" element={<Lesson2 />} />
-      </Routes>
+      <div className="w-full max-w-full flex flex-col items-center justify-center pt-6 pb-10 pl-10 pr-10">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lesson/1" element={<Lesson1 />} />
+          <Route path="/lesson/2" element={<Lesson2 />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
