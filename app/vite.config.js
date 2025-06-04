b5f25a81b5c91ac744c0bc1aac6057a84e82b807
chokopay <vazovmikola@gmail.com>
2025-06-04 21:31:18 +0200

    gh pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/UOU-learn/', 
-  plugins: [react()]
-})
+  base: '/UOU-learn/',
+  plugins: [react()],
+});
